8c13cc7b2e2655d3f9add44081a4069dbf05c3f4
Yanick Champoux <yanick@babyl.dyndns.org>
2018-12-11 15:24:37 -0500

    graphql file

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import _ from 'lodash';
-const { ApolloServer, gql } = require('apollo-server');
+import { GraphQLServer } from 'graphql-yoga';
 
-import { typeDefs as Account, resolvers as accountResolvers }
-    from './schemas/account';
+import resolver from './resolvers/account';
 
-const resolvers = {};
-
-const server = new ApolloServer({
-    typeDefs: [ Account ],
-    resolvers: _.merge( resolvers, accountResolvers ),
+const server = new GraphQLServer({
+    typeDefs: './graphql/schema.graphql',
+    resolvers: resolver,
 });
 
 // This `listen` method launches a web-server.  Existing apps
 // can utilize middleware options, which we'll discuss later.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+server.start( () => console.log(`🚀  Server ready`) );
--- src/resolvers/account.js
+++ src/resolvers/account.js
@@ -1,3 +1,4 @@
+
 const accounts = [
   {
       id: "1",
